b507a5377e2fe378f3b559051e5f89ee227c78fe
Itziar Aldabe <itziar.aldabe@ehu.es>
2014-12-04 16:15:16 +0100

    cleaning of the unused code

--- src/main/java/ixa/pipe/time/IXAPipeHeidelTime.java
+++ src/main/java/ixa/pipe/time/IXAPipeHeidelTime.java
@@ -97,7 +97,6 @@ public class IXAPipeHeidelTime{
 	/**
 	 * Logging engine
 	 */
-	//private static Logger logger = Logger.getLogger("HeidelTimeStandalone");
 	private static Logger logger = Logger.getLogger("IXAPipeHeidelTime");
 
 	
@@ -146,7 +145,6 @@ public class IXAPipeHeidelTime{
 	 * 
 	 */
 	public void initialize(Language language, DocumentType typeToProcess, String configPath){
-	//public void initialize(Language language, DocumentType typeToProcess, OutputType outputType, String configPath, POSTagger posTagger, Boolean doIntervalTagging) {
 		logger.log(Level.INFO, "IXAPipeHeidelTime initialized with language " + this.language.getName());
 
 		// read in configuration in case it's not yet initialized
@@ -256,17 +254,7 @@ public class IXAPipeHeidelTime{
 		}
 	}
 
-	/**
-	 * Establishes preconditions for jcas to be processed by HeidelTime
-	 * 
-	 * @param jcas
-	 */
-	@SuppressWarnings("unused")
-	private void establishHeidelTimePreconditions(JCas jcas) {
-		// Token information & sentence structure
-		establishPartOfSpeechInformation(jcas);
-	}
-	
+
 	private void establishHeidelTimePreconditions(JCas jcas, NAFWrapper wrapper) {
 		// Token information & sentence structure
 		logger.log(Level.FINEST, "Establishing part of speech information...");
@@ -275,60 +263,6 @@ public class IXAPipeHeidelTime{
 		} catch (AnalysisEngineProcessException e) {
 			e.printStackTrace();
 		}
-		logger.log(Level.FINEST, "Part of speech information established");
-	}
-
-	/**
-	 * Establishes part of speech information for cas object.
-	 * 
-	 * @param jcas
-	 */
-	private void establishPartOfSpeechInformation(JCas jcas) {
-		logger.log(Level.FINEST, "Establishing part of speech information...");
-
-		PartOfSpeechTagger partOfSpeechTagger = null;
-		Properties settings = new Properties();
-		switch (language) {
-			case ARABIC:
-				partOfSpeechTagger = new StanfordPOSTaggerWrapper();
-				settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_TOKENS, true);
-				settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_SENTENCES, true);
-				settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_POS, true);
-				settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_MODEL_PATH, Config.get(Config.STANFORDPOSTAGGER_MODEL_PATH));
-				settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_CONFIG_PATH, Config.get(Config.STANFORDPOSTAGGER_CONFIG_PATH));
-				break;
-			case VIETNAMESE:
-				partOfSpeechTagger = new JVnTextProWrapper();
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_ANNOTATE_TOKENS, true);
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_ANNOTATE_SENTENCES, true);
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_ANNOTATE_POS, true);
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_WORD_MODEL_PATH, Config.get(Config.JVNTEXTPRO_WORD_MODEL_PATH));
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_SENT_MODEL_PATH, Config.get(Config.JVNTEXTPRO_SENT_MODEL_PATH));
-				settings.put(PartOfSpeechTagger.JVNTEXTPRO_POS_MODEL_PATH, Config.get(Config.JVNTEXTPRO_POS_MODEL_PATH));
-				break;
-			default:
-				if(POSTagger.STANFORDPOSTAGGER.equals(posTagger)) {
-					partOfSpeechTagger = new StanfordPOSTaggerWrapper();
-					settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_TOKENS, true);
-					settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_SENTENCES, true);
-					settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_ANNOTATE_POS, true);
-					settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_MODEL_PATH, Config.get(Config.STANFORDPOSTAGGER_MODEL_PATH));
-					settings.put(PartOfSpeechTagger.STANFORDPOSTAGGER_CONFIG_PATH, Config.get(Config.STANFORDPOSTAGGER_CONFIG_PATH));
-				} else if(POSTagger.TREETAGGER.equals(posTagger)) {
-					partOfSpeechTagger = new TreeTaggerWrapper();
-					settings.put(PartOfSpeechTagger.TREETAGGER_LANGUAGE, language);
-					settings.put(PartOfSpeechTagger.TREETAGGER_ANNOTATE_TOKENS, true);
-					settings.put(PartOfSpeechTagger.TREETAGGER_ANNOTATE_SENTENCES, true);
-					settings.put(PartOfSpeechTagger.TREETAGGER_ANNOTATE_POS, true);
-					settings.put(PartOfSpeechTagger.TREETAGGER_IMPROVE_GERMAN_SENTENCES, (language == Language.GERMAN));
-					settings.put(PartOfSpeechTagger.TREETAGGER_CHINESE_TOKENIZER_PATH, Config.get(Config.CHINESE_TOKENIZER_PATH));
-				} else {
-					logger.log(Level.FINEST, "Sorry, but you can't use that tagger.");
-				}
-		}
-		partOfSpeechTagger.initialize(settings);
-		partOfSpeechTagger.process(jcas);
-
 		logger.log(Level.FINEST, "Part of speech information established");
 	}
 
@@ -355,14 +289,10 @@ public class IXAPipeHeidelTime{
 	 *             If document creation time is missing when processing a
 	 *             document of type {@link DocumentType#NEWS}
 	 */
-	public void process (KAFDocument kaf)
-	//public String process(String document, Date documentCreationTime, ResultFormatter resultFormatter)
-			throws DocumentCreationTimeMissingException {
+	public void process (KAFDocument kaf) throws DocumentCreationTimeMissingException {
 		logger.log(Level.INFO, "Processing started");
 
 		NAFWrapper wrapper = new NAFWrapper(kaf,mappingFile);
-		//ResultFormatter resultFormatter = new TimeMLResultFormatter();
-		//ResultFormatter resultFormatter = new XMIResultFormatter();
 		
 		// Generate jcas object ----------
 		logger.log(Level.FINE, "Generate CAS object");
@@ -399,11 +329,8 @@ public class IXAPipeHeidelTime{
 		
 		// Process results ---------------
 		logger.log(Level.FINE, "Formatting result...");
-		// PrintAnnotations.printAnnotations(jcas.getCas(), System.out);
-		//String result = null;
+
 		try {
-			//result = resultFormatter.format(jcas);
-			
 			format(wrapper,jcas);
 			
 			logger.log(Level.INFO, "Result formatted");
@@ -412,12 +339,9 @@ public class IXAPipeHeidelTime{
 			logger.log(Level.WARNING, "Result could not be formatted");
 		}
 
-		//return result;
 	}
 	
 	private void format (NAFWrapper wrapper, JCas jcas){
-		// INTERVALS : TO IMPLEMENT
-		
 		/* 
 		 * loop through the timexes to create two treemaps:
 		 * - one containing startingposition=>timex tuples for eradication of overlapping timexes
@@ -482,257 +406,6 @@ public class IXAPipeHeidelTime{
 		
 	}
 	
-
-	
-	/**
-	 * @param args
-	 */
-	/*
-	public static void main(String[] args) {
-		String docPath = null;
-		for(int i = 0; i < args.length; i++) { // iterate over cli parameter tokens
-			if(args[i].startsWith("-")) { // assume we found a switch
-				// get the relevant enum
-				CLISwitch sw = CLISwitch.getEnumFromSwitch(args[i]);
-				if(sw == null) { // unsupported CLI switch
-					logger.log(Level.WARNING, "Unsupported switch: "+args[i]+". Quitting.");
-					System.exit(-1);
-				}
-				
-				if(sw.getHasFollowingValue()) { // handle values for switches
-					if(args.length > i+1 && !args[i+1].startsWith("-")) { // we still have an array index after this one and it's not a switch
-						sw.setValue(args[++i]);
-					} else { // value is missing or malformed
-						logger.log(Level.WARNING, "Invalid or missing parameter after "+args[i]+". Quitting.");
-						System.exit(-1);
-					}
-				} else { // activate the value-less switches
-					sw.setValue(null);
-				}
-			} else { // assume we found the document's path/name
-				docPath = args[i];
-			}
-		}
-		
-		
-		// display help dialog if HELP-switch is given
-		if(CLISwitch.HELP.getIsActive()) {
-			printHelp();
-			System.exit(0);
-		}
-		
-		// start off with the verbosity recognition -- lots of the other 
-		// stuff can be skipped if this is set too high
-		if(CLISwitch.VERBOSITY2.getIsActive()) {
-			logger.setLevel(Level.ALL);
-			logger.log(Level.INFO, "Verbosity: '-vv'; Logging level set to ALL.");
-		} else if(CLISwitch.VERBOSITY.getIsActive()) {
-			logger.setLevel(Level.INFO);
-			logger.log(Level.INFO, "Verbosity: '-v'; Logging level set to INFO and above.");
-		} else {
-			logger.setLevel(Level.WARNING);
-			logger.log(Level.INFO, "Verbosity -v/-vv NOT FOUND OR RECOGNIZED; Logging level set to WARNING and above.");
-		}
-		
-		// Check input encoding
-		String encodingType = null;
-		if(CLISwitch.ENCODING.getIsActive()) {
-			encodingType = CLISwitch.ENCODING.getValue().toString();
-			logger.log(Level.INFO, "Encoding '-e': "+encodingType);
-		} else {
-			// Encoding type not found
-			encodingType = CLISwitch.ENCODING.getValue().toString();
-			logger.log(Level.INFO, "Encoding '-e': NOT FOUND OR RECOGNIZED; set to 'UTF-8'");
-		}
-		
-		// Check output format
-		OutputType outputType = null;
-		if(CLISwitch.OUTPUTTYPE.getIsActive()) {
-			outputType = OutputType.valueOf(CLISwitch.OUTPUTTYPE.getValue().toString().toUpperCase());
-			logger.log(Level.INFO, "Output '-o': "+outputType.toString().toUpperCase());
-		} else {
-			// Output type not found
-			outputType = (OutputType) CLISwitch.OUTPUTTYPE.getValue();
-			logger.log(Level.INFO, "Output '-o': NOT FOUND OR RECOGNIZED; set to "+outputType.toString().toUpperCase());
-		}
-		
-		// Check language
-		Language language = null;
-		if(CLISwitch.LANGUAGE.getIsActive()) {
-			language = Language.getLanguageFromString((String) CLISwitch.LANGUAGE.getValue());
-			
-			if(language == Language.WILDCARD) {
-				logger.log(Level.SEVERE, "Language '-l': "+CLISwitch.LANGUAGE.getValue()+" NOT RECOGNIZED; aborting.");
-				printHelp();
-				System.exit(-1);
-			} else {
-				logger.log(Level.INFO, "Language '-l': "+language.toString().toUpperCase());	
-			}
-		} else {
-			// Language not found
-			language = Language.getLanguageFromString((String) CLISwitch.LANGUAGE.getValue());
-			logger.log(Level.INFO, "Language '-l': NOT FOUND; set to "+language.toString().toUpperCase());
-		}
-
-		// Check type
-		DocumentType type = null;
-		if(CLISwitch.DOCTYPE.getIsActive()) {
-			type = DocumentType.valueOf(CLISwitch.DOCTYPE.getValue().toString().toUpperCase());
-			logger.log(Level.INFO, "Type '-t': "+type.toString().toUpperCase());
-		} else {
-			// Type not found
-			type = (DocumentType) CLISwitch.DOCTYPE.getValue();
-			logger.log(Level.INFO, "Type '-t': NOT FOUND OR RECOGNIZED; set to "+type.toString().toUpperCase());
-		}
-
-		// Check document creation time
-		Date dct = null;
-		if(CLISwitch.DCT.getIsActive()) {
-			try {
-				DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-				dct = formatter.parse(CLISwitch.DCT.getValue().toString());
-				logger.log(Level.INFO, "Document Creation Time '-dct': "+dct.toString());
-			} catch (Exception e) {
-				// DCT was not parseable
-				logger.log(Level.WARNING, "Document Creation Time '-dct': NOT RECOGNIZED. Quitting.");
-				printHelp();
-				System.exit(-1);
-			}
-		} else {
-			if ((type == DocumentType.NEWS) || (type == DocumentType.COLLOQUIAL)) {
-				// Dct needed
-				dct = (Date) CLISwitch.DCT.getValue();
-				logger.log(Level.INFO, "Document Creation Time '-dct': NOT FOUND; set to local date ("
-						+ dct.toString() + ").");
-			} else {
-				logger.log(Level.INFO, "Document Creation Time '-dct': NOT FOUND; skipping.");
-			}
-		}
-		
-		// Handle locale switch
-		String locale = (String) CLISwitch.LOCALE.getValue();
-		Locale myLocale = null;
-		if(CLISwitch.LOCALE.getIsActive()) {
-			// check if the requested locale is available
-			for(Locale l : Locale.getAvailableLocales()) {
-				if(l.toString().toLowerCase().equals(locale.toLowerCase()))
-					myLocale = l;
-			}
-			
-			try {
-				Locale.setDefault(myLocale); // try to set the locale
-				logger.log(Level.INFO, "Locale '-locale': "+myLocale.toString());
-			} catch(Exception e) { // if the above fails, spit out error message and available locales
-				logger.log(Level.WARNING, "Supplied locale parameter couldn't be resolved to a working locale. Try one of these:");
-				logger.log(Level.WARNING, Arrays.asList(Locale.getAvailableLocales()).toString()); // list available locales
-				printHelp();
-				System.exit(-1);
-			}
-		} else {
-			// no -locale parameter supplied: just show default locale
-			logger.log(Level.INFO, "Locale '-locale': NOT FOUND, set to environment locale: "+Locale.getDefault().toString());
-		}
-		
-		// Read configuration from file
-		String configPath = CLISwitch.CONFIGFILE.getValue().toString();
-		try {
-			logger.log(Level.INFO, "Configuration path '-c': "+configPath);
-
-			readConfigFile(configPath);
-
-			logger.log(Level.FINE, "Config initialized");
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.log(Level.WARNING, "Config could not be initialized! Please supply the -c switch or "
-					+ "put a config.props into this directory.");
-			printHelp();
-			System.exit(-1);
-		}
-
-		// Set the preprocessing POS tagger
-		POSTagger posTagger = null;
-		if(CLISwitch.POSTAGGER.getIsActive()) {
-			try {
-				posTagger = POSTagger.valueOf(CLISwitch.POSTAGGER.getValue().toString().toUpperCase());
-			} catch(IllegalArgumentException e) {
-				logger.log(Level.WARNING, "Given POS Tagger doesn't exist. Please specify a valid one as listed in the help.");
-				printHelp();
-				System.exit(-1);
-			}
-			logger.log(Level.INFO, "POS Tagger '-pos': "+posTagger.toString().toUpperCase());
-		} else {
-			// Type not found
-			posTagger = (POSTagger) CLISwitch.POSTAGGER.getValue();
-			logger.log(Level.INFO, "POS Tagger '-pos': NOT FOUND OR RECOGNIZED; set to "+posTagger.toString().toUpperCase());
-		}
-
-		// Set whether or not to use the Interval Tagger
-		Boolean doIntervalTagging = false;
-		if(CLISwitch.INTERVALS.getIsActive()) {
-			doIntervalTagging = CLISwitch.INTERVALS.getIsActive();
-			logger.log(Level.INFO, "Interval Tagger '-it': " + doIntervalTagging.toString());
-		} else {
-			logger.log(Level.INFO, "Interval Tagger '-it': NOT FOUND OR RECOGNIZED; set to " + doIntervalTagging.toString());
-		}
-		
-		// make sure we have a document path
-		if (docPath == null) {
-			logger.log(Level.WARNING, "No input file given; aborting.");
-			printHelp();
-			System.exit(-1);
-		}
-		
-		
-
-		// Run HeidelTime
-		RandomAccessFile aFile = null;
-		MappedByteBuffer buffer = null;
-		FileChannel inChannel = null;
-		PrintWriter pwOut = null;
-		try {
-			logger.log(Level.INFO, "Reading document using charset: " + encodingType);
-			
-			aFile = new RandomAccessFile(docPath, "r");
-			inChannel = aFile.getChannel();
-			buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-			buffer.load();
-			byte[] inArr = new byte[(int) inChannel.size()];
-			
-			for(int i = 0; i < buffer.limit(); i++) {
-				inArr[i] = buffer.get();
-			}
-			
-			// double-newstring should not be necessary, but without this, it's not running on Windows (?)
-			String input = new String(new String(inArr, encodingType).getBytes("UTF-8"), "UTF-8");
-			
-			HeidelTimeStandalone standalone = new HeidelTimeStandalone(language, type, outputType, null, posTagger, doIntervalTagging);
-			String out = standalone.process(input, dct);
-			
-			// Print output always as UTF-8
-			pwOut = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"));
-			pwOut.println(out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if(pwOut != null) {
-				pwOut.close();
-			}
-			if(buffer != null) {
-				buffer.clear();
-			}
-			if(inChannel != null) {
-				try {
-					inChannel.close();
-				} catch (IOException e) { }
-			}
-			if(aFile != null) {
-				try {
-					aFile.close();
-				} catch (IOException e) { }
-			}
-		}
-	}
-	*/
 	
 	public static void readConfigFile(String configPath) {
 		InputStream configStream = null;
